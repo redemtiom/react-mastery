5349115eb8cea7749582f2ae9d2ef5586a8ef831
redemtiom <smar.pre.sky@gmail.com>
2020-05-17 10:35:57 -0500

    Updates

---
+++ precache-manifest.c957e219e662c71e072d7663e479ec50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976f341690358134a9a435067b3981dc",
+    "url": "/react-mastery/index.html"
+  },
+  {
+    "revision": "43df92e3d05d3f054c7d",
+    "url": "/react-mastery/static/css/main.adfa5b21.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/react-mastery/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-mastery/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43df92e3d05d3f054c7d",
+    "url": "/react-mastery/static/js/main.834f4ae2.chunk.js"
+  },
+  {
+    "revision": "e47d5332fcbb635b3e72",
+    "url": "/react-mastery/static/js/runtime-main.170d8dfd.js"
+  }
+]);
